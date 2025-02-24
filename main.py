331a864b1beb4438e1bab4469c58abca6630ced1
truwsfff <138609244+truwsfff@users.noreply.github.com>
2025-02-24 16:24:46 +0300

    task1

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/')
-@app.route('/index')
-def login():
-    return render_template('base.html', title='Заготовка')
+@app.route('/<title>')
+@app.route('/index/<title>')
+def login(title):
+    return render_template('base.html', title=title)
 
 
 if __name__ == '__main__':
